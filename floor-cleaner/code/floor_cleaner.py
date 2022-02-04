fed928f44216601b0c23660fe4be6ea2a3995aa1
Sainithinkadarla <my.life496s@gmail.com>
2022-02-04 10:34:52 +0530

    Update floor_cleaner.py

--- floor-cleaner/code/floor_cleaner.py
+++ floor-cleaner/code/floor_cleaner.py
@@ -1,150 +1,149 @@
-from machine import Pin,PWM #importing Pin module from machine package
-from time import sleep #importing sleep module from time package
-import utime #importing utime module for micro seconds
-	 
-#setting up motor pins 
-MotorL1 = Pin(3,Pin.OUT)
-MotorL2 = Pin(4,Pin.OUT)
-MotorR1 = Pin(5,Pin.OUT)
-MotorR2 = Pin(6,Pin.OUT)
+from machine import Pin,PWM#importing Pin module from machine package
+from time import sleep#importing sleep module from time package
+import utime#importing utime module for micro seconds
+
+
+MotorL1 = Pin(6,Pin.OUT)
+MotorL2 = Pin(5,Pin.OUT)
+MotorR1 = Pin(4,Pin.OUT)
+MotorR2 = Pin(3,Pin.OUT)
 MotorEN = Pin(2,Pin.OUT)
-	
-#setting up ultrasonic distance sensor pins 
+
 trigger = Pin(14, Pin.OUT)
 echo = Pin(15, Pin.IN)
- 
-#microseconds for servo motor rotation
+
 MID=1300000
-MIN=350000
+MIN=0250000
 MAX=2300000
-	
-#setting up servo motor PWM pin
+
 pwm=PWM(Pin(1))
 pwm.freq(50)
 pwm.duty_ns(MID)
-	 
+
 def get_distance(str):
-   trigger.low() #making trigger low to reset
-   echo.low() #making echo low to reset 
+   trigger.low()
+   echo.low()
    print("distance fetching started")
-   trigger.low() #makingtrigger low for calculation 
-   utime.sleep_us(2) #waits for 2 microseconds
-   trigger.high() #making trigger high 
-   utime.sleep_us(5) #waits for 2 microseconds
-   trigger.low() #making trigger low
-   
+   trigger.low()
+   utime.sleep_us(2)
+   trigger.high()
+   utime.sleep_us(5)
+   trigger.low()
+   #timepassed=0
    print("distance fetching started")
    
-   while echo.value() == 0: #checks echo as value or not
-       signaloff = utime.ticks_us() #stores initial micro time
-   while echo.value() == 1: #checks echo as value or not
-       signalon = utime.ticks_us() #stores final micro time
+   while echo.value() == 0:
+       signaloff = utime.ticks_us()
+   while echo.value() == 1:
+       signalon = utime.ticks_us()
    print("distance fetching started")
-   timepassed = signalon-signaloff #calculates actual time
+   timepassed = signalon - signaloff
    
-   global distance #making distance variable as global 
-   distance = (timepassed * 0.0343) / 2 #calculating distance
-   print("The distance from object is ",distance,"cm",str) 
-   return distance #returns distance
- 
-def m_forward(): #moves forward
+   global distance
+   distance = (timepassed * 0.0343) / 2
+   print("The distance from object is ",distance,"cm",str)
+   return distance
+
+def m_forward():
     print('Forward')
     MotorL1.value(1)
     MotorL2.value(0)
     MotorR1.value(1)
     MotorR2.value(0)
     MotorEN.value(1)
-	 
-def m_reverse(): #moves backward
+
+def m_reverse():
     print('Reverse')
     MotorL1.value(0)
     MotorL2.value(1)
     MotorR1.value(0)
     MotorR2.value(1)
     MotorEN.value(1)
-	 
-def m_right(): #turnss right
+
+def m_right():
     print('Right')
     MotorL1.value(1)
     MotorL2.value(0)
     MotorR1.value(0)
     MotorR2.value(1)
     MotorEN.value(1)
-	    
-def m_left(): #turns left 
+    
+def m_left():
     print('Left')
     MotorL1.value(0)
     MotorL2.value(1)
     MotorR1.value(1)
     MotorR2.value(0)
     MotorEN.value(1)
-	 
-def m_stop(): #stop 
+
+def m_stop():
     print('Stop')
     MotorL1.value(0)
     MotorL2.value(0)
     MotorR1.value(0)
     MotorR2.value(0)
     MotorEN.value(0)
-	 
-def s_front(): #servo motor will face forward
+
+def s_front():
     pwm.duty_ns(MID)
     sleep(2)
- 
-def s_right(): #servo motor will turn right
+
+def s_right():
     pwm.duty_ns(MIN)
     sleep(2)
-	 
-def s_left(): #servo motor will turn left
+
+def s_left():
     pwm.duty_ns(MAX)
     sleep(2)
-	 
-	 
-def inactive(): #inactives all pins or makes all pins low
+
+
+def inactive():
     m_stop()
     pwm.duty_ns(0)
     trigger.low()
     echo.low()
-    
-	 
-	 
-inactive() #making all pins low
-sleep(2) #waits for 2 seconds
-m_forward() #moves forward
-	 
-	 
-	 
-while True: 
 
-    get_distance("front") #calculating front distance
+
+inactive()
+sleep(2)
+m_forward()#
+
+
+
+while True:
+    #inactive()
+    get_distance("front")
     print("front")
-    if distance<=5: #checks if distance 
-       m_stop() #stop 
-       s_left() #servo motor turns left
-       l_dist=get_distance("left") #calculating left distance
-       sleep(1) #waits for 2 seconds
-       s_right() #servo motor turns right
-       r_dist=get_distance("right") #calculating right distance
-       s_front() #servo motor will face forward
-       if l_dist>r_dist: #checks left distance is greater than right distance
+    if distance<=7:
+        m_stop()#
+        s_left()
+        l_dist=get_distance("left")
+        sleep(1)
+        s_right()
+        r_dist=get_distance("right")
+        s_front()
+        if l_dist>r_dist:
             print("Move for 2 sec")
-            m_left() #turns left
-            sleep(2) #waits for 2 seconds
+            m_left()#
+            sleep(1)
             print("move left")
-            m_left() #turns left
-            m_forward() #moves forward
-            f_dist=get_distance("front") #calculating front distance
-       else:
+            m_left()#
+            m_forward()
+            f_dist=get_distance("front")
+            
+        else:
             print("Move for 2 sec")
-            m_right() #turns right
-            sleep(2) #waits for 2 seconds
+            m_right()#
+            sleep(1)
             print("move right")
-            m_right() #turns right
-            m_forward() #moves forward
+            m_right()#
+            m_forward()
             print("front")
-            f_dist=get_distance("front") #calculating front distance
-	 
+            f_dist=get_distance("front")
+
     else:
-        m_forward() #moves forward
+        m_forward()
 else:
-    m_forward() #moves forward
+    m_forward()
+        
+
